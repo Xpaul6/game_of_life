a9ccc5fd5e81b121e93e64fd7f816394efcff236
Pavel Kholkin <xpaul6@gmail.com>
2025-06-30 23:48:06 +0500

    ref: dedicated struct for game state

--- main.c
+++ main.c
@@ -18,22 +18,29 @@ typedef struct {
    State nextState; 
 } Cell;
 
-void init_grid(Cell** grid, int COLS, int ROWS) {
-    for (int i = 0; i < ROWS; i++) {
-        grid[i] = malloc(COLS * sizeof(Cell));
+typedef struct {
+    Cell** grid;
+    int rows;
+    int cols;
+    int speed;
+} Game;
+
+void init_grid(Game* game) {
+    for (int i = 0; i < game->rows; i++) {
+        game->grid[i] = malloc(game->cols * sizeof(Cell));
     }
-    for (int y = 0; y < ROWS; y++) {
-        for (int x = 0; x < COLS; x++) {
-            grid[y][x].currentState = DEAD;
-            grid[y][x].nextState = DEAD;
+    for (int y = 0; y < game->rows; y++) {
+        for (int x = 0; x < game->cols; x++) {
+            game->grid[y][x].currentState = DEAD;
+            game->grid[y][x].nextState = DEAD;
         }
     }
 }
 
-void print_grid(Cell** grid, int COLS, int ROWS) {
-    for (int y = 0; y < ROWS; y++) {
-        for (int x = 0; x < COLS; x++) {
-            if (grid[y][x].currentState == DEAD) {
+void print_grid(Game* game) {
+    for (int y = 0; y < game->rows; y++) {
+        for (int x = 0; x < game->cols; x++) {
+            if (game->grid[y][x].currentState == DEAD) {
                 printf(SYM_DEAD);
             } else {
                 printf(SYM_ALIVE);
@@ -43,82 +50,84 @@ void print_grid(Cell** grid, int COLS, int ROWS) {
     }
 }
 
-void update_grid(Cell** grid, int COLS, int ROWS, int *alive_counter) {
-    for (int y = 0; y < ROWS; y++) {
-        for (int x = 0; x < COLS; x++) {
+void update_grid(Game* game, int* alive_counter) {
+    for (int y = 0; y < game->rows; y++) {
+        for (int x = 0; x < game->cols; x++) {
             int neighbor_counter = 0;
             for (int dy = -1; dy <= 1; dy++) {
                 for (int dx = -1; dx <= 1; dx++) {
                     if (dy == 0 && dx == 0) continue;
-                    int row = (y + dy + ROWS) % ROWS;
-                    int col = (x + dx + COLS) % COLS;
-                    if (grid[row][col].currentState == ALIVE) {
+                    int row = (y + dy + game->rows) % game->rows;
+                    int col = (x + dx + game->cols) % game->cols;
+                    if (game->grid[row][col].currentState == ALIVE) {
                         neighbor_counter++;
                     }
                 }
             }
 
             // Game rules
-            if (grid[y][x].currentState == ALIVE) {
+            if (game->grid[y][x].currentState == ALIVE) {
                 if (neighbor_counter < 2) {
-                    grid[y][x].nextState = DEAD;
+                    game->grid[y][x].nextState = DEAD;
                 } else if (neighbor_counter < 4) {
-                    grid[y][x].nextState = ALIVE;
+                    game->grid[y][x].nextState = ALIVE;
                 } else {
-                    grid[y][x].nextState = DEAD;
+                    game->grid[y][x].nextState = DEAD;
                 }
             } else {
                 if (neighbor_counter == 3) {
-                    grid[y][x].nextState = ALIVE;
+                    game->grid[y][x].nextState = ALIVE;
                 } else {
-                    grid[y][x].nextState = DEAD;
+                    game->grid[y][x].nextState = DEAD;
                 }
             }
         }
     }
     int local_alive_counter = 0;
-    for (int y = 0; y < ROWS; y++) {
-        for (int x = 0; x < COLS; x++) {
-            grid[y][x].currentState = grid[y][x].nextState;
-            if (grid[y][x].currentState == ALIVE) local_alive_counter++;
+    for (int y = 0; y < game->rows; y++) {
+        for (int x = 0; x < game->cols; x++) {
+            game->grid[y][x].currentState = game->grid[y][x].nextState;
+            if (game->grid[y][x].currentState == ALIVE) local_alive_counter++;
         }
     }
     *alive_counter = local_alive_counter;
 }
 
-void draw_acorn(Cell** grid, int COLS, int ROWS);
-void draw_glider(Cell** grid, int COLS, int ROWS); 
-void draw_r_pentomino(Cell** grid, int COLS, int ROWS);
-void draw_diehart(Cell** grid, int COLS, int ROWS);
+void draw_acorn(Game* game);
+void draw_glider(Game* game); 
+void draw_r_pentomino(Game* game);
+void draw_diehart(Game* game);
 
 // entry point
 int main(int argc, char** argv) {
-    int ROWS = 35;
-    int COLS = 130;
-    int SPEED = 10;
+    Game game = {0};
+
+    game.rows = 35;
+    game.cols = 130;
+    game.speed = 10;
 
     if (argc == 5) {
-        COLS = strtol(argv[1], NULL, 10);
-        ROWS = strtol(argv[2], NULL, 10);
-        SPEED = strtol(argv[3], NULL, 10);
-        if (COLS < 10 || ROWS < 10 || SPEED < 1) {
+        game.cols = strtol(argv[1], NULL, 10);
+        game.rows = strtol(argv[2], NULL, 10);
+        game.speed = strtol(argv[3], NULL, 10);
+        if (game.cols < 10 || game.rows < 10 || game.speed < 1) {
             printf("Minimal grid size is 10x10, minimal speed is 1 tick/s\nExiting...\n");
             return -1;
         }
     }
 
-    Cell** grid = (Cell**)malloc(ROWS * COLS);
+    game.grid = malloc(game.rows * sizeof(Cell*));
 
-    init_grid(grid, COLS, ROWS);
+    init_grid(&game);
 
     if (!strcmp(argv[4], "glider")) {
-        draw_glider(grid, COLS, ROWS);
+        draw_glider(&game);
     } else if (!strcmp(argv[4], "diehart")) {
-        draw_diehart(grid, COLS, ROWS);
+        draw_diehart(&game);
     } else if (!strcmp(argv[4], "r-pentomino")) {
-        draw_r_pentomino(grid, COLS, ROWS);
+        draw_r_pentomino(&game);
     } else {
-        draw_acorn(grid, COLS, ROWS);
+        draw_acorn(&game);
     }
 
     int generation_counter = 1;
@@ -128,59 +137,59 @@ int main(int argc, char** argv) {
 
     while (alive_counter != 0) {
         printf("Generation: %d\n", generation_counter);
-        print_grid(grid, COLS, ROWS);
+        print_grid(&game);
 
-        usleep(1000000 / SPEED);
+        usleep(1000000 / game.speed);
         printf(ESCAPE_CODE);
 
-        update_grid(grid, COLS, ROWS, &alive_counter);
+        update_grid(&game, &alive_counter);
         generation_counter++;
     }
 
     return 0;
 }
 
-void draw_acorn(Cell** grid, int COLS, int ROWS) {
-    int c_row = ROWS / 2;
-    int c_col = COLS / 2;
+void draw_acorn(Game* game) {
+    int c_row = game->rows / 2;
+    int c_col = game->cols / 2;
 
-    grid[c_row][c_col].currentState = ALIVE;
-    grid[c_row + 1][c_col + 1].currentState = ALIVE;
-    grid[c_row + 1][c_col + 2].currentState = ALIVE;
-    grid[c_row + 1][c_col + 3].currentState = ALIVE;
-    grid[c_row - 1][c_col - 2].currentState = ALIVE;
-    grid[c_row + 1][c_col - 2].currentState = ALIVE;
-    grid[c_row + 1][c_col - 3].currentState = ALIVE;
+    game->grid[c_row][c_col].currentState = ALIVE;
+    game->grid[c_row + 1][c_col + 1].currentState = ALIVE;
+    game->grid[c_row + 1][c_col + 2].currentState = ALIVE;
+    game->grid[c_row + 1][c_col + 3].currentState = ALIVE;
+    game->grid[c_row - 1][c_col - 2].currentState = ALIVE;
+    game->grid[c_row + 1][c_col - 2].currentState = ALIVE;
+    game->grid[c_row + 1][c_col - 3].currentState = ALIVE;
 }
 
-void draw_glider(Cell** grid, int COLS, int ROWS) {
-    grid[1][1].currentState = ALIVE;
-    grid[2][2].currentState = ALIVE;
-    grid[3][0].currentState = ALIVE;
-    grid[3][1].currentState = ALIVE;
-    grid[3][2].currentState = ALIVE;
+void draw_glider(Game* game) {
+    game->grid[1][1].currentState = ALIVE;
+    game->grid[2][2].currentState = ALIVE;
+    game->grid[3][0].currentState = ALIVE;
+    game->grid[3][1].currentState = ALIVE;
+    game->grid[3][2].currentState = ALIVE;
 }
 
-void draw_r_pentomino(Cell** grid, int COLS, int ROWS) {
-    int c_row = ROWS / 2;
-    int c_col = COLS / 2;
-    grid[c_row][c_col].currentState = ALIVE;
-    grid[c_row - 1][c_col].currentState = ALIVE;
-    grid[c_row + 1][c_col].currentState = ALIVE;
-    grid[c_row][c_col - 1].currentState = ALIVE;
-    grid[c_row + 1][c_col + 1].currentState = ALIVE;
+void draw_r_pentomino(Game* game) {
+    int c_row = game->rows / 2;
+    int c_col = game->cols / 2;
+    game->grid[c_row][c_col].currentState = ALIVE;
+    game->grid[c_row - 1][c_col].currentState = ALIVE;
+    game->grid[c_row + 1][c_col].currentState = ALIVE;
+    game->grid[c_row][c_col - 1].currentState = ALIVE;
+    game->grid[c_row + 1][c_col + 1].currentState = ALIVE;
 
 }
 
-void draw_diehart(Cell** grid, int COLS, int ROWS) {
-    int c_row = ROWS / 2;
-    int c_col = COLS / 2;
+void draw_diehart(Game* game) {
+    int c_row = game->rows / 2;
+    int c_col = game->cols / 2;
 
-    grid[c_row][c_col - 3].currentState = ALIVE;
-    grid[c_row][c_col - 2].currentState = ALIVE;
-    grid[c_row + 1][c_col - 3].currentState = ALIVE;
-    grid[c_row - 1][c_col + 3].currentState = ALIVE;
-    grid[c_row + 1][c_col + 2].currentState = ALIVE;
-    grid[c_row + 1][c_col + 3].currentState = ALIVE;
-    grid[c_row + 1][c_col + 4].currentState = ALIVE;
+    game->grid[c_row][c_col - 3].currentState = ALIVE;
+    game->grid[c_row][c_col - 2].currentState = ALIVE;
+    game->grid[c_row + 1][c_col - 3].currentState = ALIVE;
+    game->grid[c_row - 1][c_col + 3].currentState = ALIVE;
+    game->grid[c_row + 1][c_col + 2].currentState = ALIVE;
+    game->grid[c_row + 1][c_col + 3].currentState = ALIVE;
+    game->grid[c_row + 1][c_col + 4].currentState = ALIVE;
 }
